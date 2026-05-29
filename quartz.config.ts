244de57bf855d8a8fb94aa875a3f2889acfdc178
Brendan DH <brendan.devlin-hill@cern.ch>
2026-05-29 22:40:08 +0200

    Quartz sync: May 29, 2026, 10:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-IE",
+    locale: "en-GB",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
